88d3b7990562f70122cd723b21075ac2ca8909f5
Henrik Nilsson <henrik@lhjnilsson.com>
2024-04-27 15:47:49 +0200

    split session into automated and manual

--- backtest/internal/backtest/session.go
+++ backtest/internal/backtest/session.go
@@ -2,245 +2,80 @@ package backtest
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/lhjnilsson/foreverbull/backtest/entity"
 	"github.com/lhjnilsson/foreverbull/backtest/internal/repository"
-	"github.com/lhjnilsson/foreverbull/internal/environment"
 	"github.com/lhjnilsson/foreverbull/service/backtest"
 	service "github.com/lhjnilsson/foreverbull/service/entity"
-	"github.com/lhjnilsson/foreverbull/service/message"
 	"github.com/lhjnilsson/foreverbull/service/socket"
 	"github.com/lhjnilsson/foreverbull/service/worker"
-	"github.com/rs/zerolog/log"
-	"go.nanomsg.org/mangos/v3"
 )
 
 type Session interface {
-	GetSocket() *socket.Socket
 	Run(chan<- bool, <-chan bool) error
-	RunExecution(ctx context.Context, execution *entity.Execution) error
 	Stop(ctx context.Context) error
 }
 
 type session struct {
-	backtest         *entity.Backtest `json:"-"`
-	session          *entity.Session
-	backtestInstance *service.Instance     `json:"-"`
-	executions       *repository.Execution `json:"-"`
-	periods          *repository.Period    `json:"-"`
+	backtest *entity.Backtest `json:"-"`
+	session  *entity.Session
 
-	b       backtest.Backtest `json:"-"`
-	workers worker.Pool       `json:"-"`
-
-	Socket          socket.Socket        `json:"-"`
-	socket          socket.ContextSocket `json:"-"`
-	execution       Execution            `json:"-"`
-	executionEntity *entity.Execution    `json:"-"`
+	executions *repository.Execution `json:"-"`
+	periods    *repository.Period    `json:"-"`
 }
 
 func NewSession(ctx context.Context,
 	storedBacktest *entity.Backtest, storedSession *entity.Session, backtestInstance *service.Instance,
-	executions *repository.Execution, periods *repository.Period, workers ...*service.Instance) (Session, error) {
+	executions *repository.Execution, periods *repository.Period, workers ...*service.Instance) (Session, *socket.Socket, error) {
 	b, err := backtest.NewZiplineEngine(ctx, backtestInstance)
 	if err != nil {
-		return nil, fmt.Errorf("error creating zipline engine: %w", err)
+		return nil, nil, fmt.Errorf("error creating zipline engine: %w", err)
 	}
 	workerPool, err := worker.NewPool(ctx, workers...)
 	if err != nil {
-		return nil, err
+		return nil, nil, fmt.Errorf("error creating worker pool: %w", err)
 	}
-
 	s := session{
-		backtest:         storedBacktest,
-		session:          storedSession,
-		backtestInstance: backtestInstance,
-		executions:       executions,
-		periods:          periods,
+		backtest: storedBacktest,
+		session:  storedSession,
 
-		b:       b,
-		workers: workerPool,
-	}
-	s.Socket = socket.Socket{Type: socket.Replier, Host: "0.0.0.0", Port: 0, Listen: true, Dial: false}
-	s.socket, err = socket.GetContextSocket(ctx, &s.Socket)
-	if err != nil {
-		return nil, err
-	}
-	return &s, b.DownloadIngestion(ctx, storedBacktest.Name)
-}
-
-func (e *session) GetSocket() *socket.Socket {
-	return &e.Socket
-}
-
-func (e *session) RunExecution(ctx context.Context, execution *entity.Execution) error {
-	exec := NewExecution(e.b, e.workers)
-
-	workerCfg := worker.Configuration{
-		Execution: execution.ID,
-		Port:      e.workers.SocketConfig().Port,
-		Database:  environment.GetPostgresURL(),
+		executions: executions,
+		periods:    periods,
 	}
 
-	tz := "UTC"
-	backtestCfg := backtest.BacktestConfig{
-		Calendar:  &execution.Calendar,
-		Start:     &execution.Start,
-		End:       &execution.End,
-		Timezone:  &tz,
-		Benchmark: execution.Benchmark,
-		Symbols:   &execution.Symbols,
-	}
-
-	err := exec.Configure(ctx, &workerCfg, &backtestCfg)
-	if err != nil {
-		return fmt.Errorf("failed to configure execution: %w", err)
-	}
-
-	err = e.executions.UpdateParameters(ctx, execution.ID, &execution.Parameters)
-	if err != nil {
-		return fmt.Errorf("failed to update execution parameters: %w", err)
-	}
-	err = e.executions.UpdateSimulationDetails(ctx, execution.ID, *backtestCfg.Calendar, *backtestCfg.Start, *backtestCfg.End, backtestCfg.Benchmark, *backtestCfg.Symbols)
-	if err != nil {
-		return fmt.Errorf("failed to update execution simulation details: %w", err)
-	}
-
-	err = exec.Run(context.TODO(), execution)
-	if err != nil {
-		return fmt.Errorf("failed to run execution: %w", err)
-	}
-	periods, err := exec.StoreDataFrameAndGetPeriods(context.Background(), execution.ID)
-	if err != nil {
-		return err
-	}
-	for _, period := range *periods {
-		err = e.periods.Store(context.Background(), execution.ID, &period)
+	if storedSession.Manual {
+		sock := socket.Socket{Type: socket.Replier, Host: "0.0.0.0", Port: 0, Listen: true, Dial: false}
+		socket, err := socket.GetContextSocket(ctx, &sock)
 		if err != nil {
-			return err
+			return nil, nil, fmt.Errorf("failed to get socket: %w", err)
 		}
+
+		ms := manualSession{
+			session: s,
+
+			backtest: b,
+			workers:  workerPool,
+
+			Socket: sock,
+			socket: socket,
+		}
+		return &ms, &sock, b.DownloadIngestion(ctx, storedBacktest.Name)
+	} else {
+		executions, err := executions.ListBySession(ctx, storedSession.ID)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to list executions: %w", err)
+		}
+
+		as := automatedSession{
+			session: s,
+
+			backtest: b,
+			workers:  workerPool,
+
+			executions: executions,
+		}
+		return &as, nil, b.DownloadIngestion(ctx, storedBacktest.Name)
+
 	}
-	return nil
-}
-
-func (e *session) Run(activity chan<- bool, stop <-chan bool) error {
-	defer func() {
-		err := e.workers.Stop(context.TODO())
-		if err != nil {
-			log.Err(err).Msg("failed to stop workers")
-		}
-		err = e.b.Stop(context.TODO())
-		if err != nil {
-			log.Err(err).Msg("failed to stop engine")
-		}
-		err = e.socket.Close()
-		if err != nil {
-			log.Err(err).Msg("failed to close socket")
-		}
-	}()
-	for {
-		socket, err := e.socket.Get()
-		if err != nil {
-			return fmt.Errorf("failed to get socket: %w", err)
-		}
-		defer socket.Close()
-		var rsp message.Response
-		byteMsg, err := socket.Read()
-		if err != nil {
-			if err == mangos.ErrRecvTimeout {
-				select {
-				case <-stop:
-					return fmt.Errorf("received stop signal")
-				default:
-					continue
-				}
-			}
-			return err
-		}
-		activity <- true
-		req := message.Request{}
-		if err = req.Decode(byteMsg); err != nil {
-			continue
-		}
-		rsp = message.Response{Task: req.Task}
-		switch req.Task {
-		case "new_execution":
-			if e.execution != nil {
-				err = errors.New("execution already exists")
-				break
-			}
-			e.executionEntity, err = e.executions.Create(context.Background(), e.session.ID, e.backtest.Calendar, e.backtest.Start, e.backtest.End, e.backtest.Symbols, e.backtest.Benchmark)
-			if err != nil {
-				err = errors.New("failed to create execution")
-				break
-			}
-			e.executionEntity.Database = environment.GetPostgresURL()
-
-			e.executionEntity.Port = &e.workers.SocketConfig().Port
-
-			e.execution = NewExecution(e.b, e.workers)
-			rsp = message.Response{Task: req.Task, Data: e.executionEntity}
-		case "run_execution":
-			tz := "UTC"
-			backtestCfg := backtest.BacktestConfig{
-				Calendar:  &e.executionEntity.Calendar,
-				Start:     &e.executionEntity.Start,
-				End:       &e.executionEntity.End,
-				Timezone:  &tz,
-				Benchmark: e.executionEntity.Benchmark,
-				Symbols:   &e.executionEntity.Symbols,
-			}
-			err = e.execution.Configure(context.Background(), nil, &backtestCfg)
-			if err != nil {
-				return fmt.Errorf("failed to configure execution: %w", err)
-			}
-			err = e.execution.Run(context.TODO(), e.executionEntity)
-			if err != nil {
-				return fmt.Errorf("failed to run execution: %w", err)
-			}
-			if rsp.Error == "" {
-				periods, err := e.execution.StoreDataFrameAndGetPeriods(context.Background(), e.executionEntity.ID)
-				if err != nil {
-					return fmt.Errorf("failed to store data frame and get periods: %w", err)
-				}
-				for _, period := range *periods {
-					err = e.periods.Store(context.Background(), e.executionEntity.ID, &period)
-					if err != nil {
-						return fmt.Errorf("failed to store period: %w", err)
-					}
-				}
-			}
-			e.execution = nil
-			e.executionEntity = nil
-		case "stop":
-			byteMsg, err = rsp.Encode()
-			if err != nil {
-				return fmt.Errorf("failed to encode message: %w", err)
-			}
-			if err = socket.Write(byteMsg); err != nil {
-				return fmt.Errorf("failed to write message: %w", err)
-			}
-			return nil
-		default:
-			err = errors.New("unknown task")
-		}
-		if err != nil {
-			rsp.Error = err.Error()
-		}
-		byteMsg, err = rsp.Encode()
-		if err != nil {
-			return fmt.Errorf("failed to encode message: %w", err)
-		}
-		if err = socket.Write(byteMsg); err != nil {
-			return fmt.Errorf("failed to write message: %w", err)
-		}
-	}
-}
-
-func (e *session) Stop(ctx context.Context) error {
-	if err := e.workers.Stop(ctx); err != nil {
-		return fmt.Errorf("failed to stop workers: %w", err)
-	}
-	return e.socket.Close()
 }
---
+++ backtest/internal/backtest/session_manual.go
@@ -0,0 +1,169 @@
+package backtest
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/lhjnilsson/foreverbull/backtest/entity"
+	"github.com/lhjnilsson/foreverbull/internal/environment"
+	"github.com/lhjnilsson/foreverbull/service/backtest"
+	service "github.com/lhjnilsson/foreverbull/service/entity"
+	"github.com/lhjnilsson/foreverbull/service/message"
+	"github.com/lhjnilsson/foreverbull/service/socket"
+	"github.com/lhjnilsson/foreverbull/service/worker"
+	"github.com/rs/zerolog/log"
+	"go.nanomsg.org/mangos/v3"
+)
+
+type manualSession struct {
+	session session `json:"-"`
+
+	backtest backtest.Backtest `json:"-"`
+	workers  worker.Pool       `json:"-"`
+
+	Socket socket.Socket        `json:"-"`
+	socket socket.ContextSocket `json:"-"`
+
+	execution       Execution         `json:"-"`
+	executionEntity *entity.Execution `json:"-"`
+}
+
+func (ms *manualSession) Run(activity chan<- bool, stop <-chan bool) error {
+	defer func() {
+		err := ms.workers.Stop(context.TODO())
+		if err != nil {
+			log.Err(err).Msg("failed to stop workers")
+		}
+		err = ms.backtest.Stop(context.TODO())
+		if err != nil {
+			log.Err(err).Msg("failed to stop engine")
+		}
+		err = ms.socket.Close()
+		if err != nil {
+			log.Err(err).Msg("failed to close socket")
+		}
+	}()
+	for {
+		socket, err := ms.socket.Get()
+		if err != nil {
+			return fmt.Errorf("failed to get socket: %w", err)
+		}
+		defer socket.Close()
+		var rsp message.Response
+		byteMsg, err := socket.Read()
+		if err != nil {
+			if err == mangos.ErrRecvTimeout {
+				select {
+				case <-stop:
+					return fmt.Errorf("received stop signal")
+				default:
+					continue
+				}
+			}
+			return err
+		}
+		activity <- true
+		req := message.Request{}
+		if err = req.Decode(byteMsg); err != nil {
+			continue
+		}
+		rsp = message.Response{Task: req.Task}
+		switch req.Task {
+		case "new_execution":
+			if ms.execution != nil {
+				err = errors.New("execution already exists")
+				break
+			}
+			type NewExecutionEntry struct {
+				execution *entity.Execution
+				service   *service.Service
+			}
+			var ne NewExecutionEntry
+			err = req.DecodeData(&ne)
+			if err != nil {
+				err = errors.New("failed to decode data")
+				break
+			}
+			if ne.execution == nil {
+				err = errors.New("execution is not specified, cannot create execution")
+				break
+			}
+			if ne.service == nil {
+				err = errors.New("service is not specified, cannot create execution")
+				break
+			}
+			ms.executionEntity, err = ms.session.executions.Create(context.Background(), ms.session.session.ID, ne.execution.Calendar, ne.execution.Start, ne.execution.End, ne.execution.Symbols, ne.execution.Benchmark)
+			if err != nil {
+				err = fmt.Errorf("failed to create execution: %w", err)
+				break
+			}
+			ms.executionEntity.Database = environment.GetPostgresURL()
+			ms.executionEntity.Port = &ms.workers.SocketConfig().Port
+			ms.execution = NewExecution(ms.backtest, ms.workers)
+			rsp = message.Response{Task: req.Task, Data: ms.executionEntity}
+		case "run_execution":
+			backtestCfg := backtest.BacktestConfig{
+				Calendar: &ms.executionEntity.Calendar,
+				Start:    &ms.executionEntity.Start,
+				End:      &ms.executionEntity.End,
+				Timezone: func() *string {
+					tz := "UTC"
+					return &tz
+				}(),
+				Benchmark: ms.executionEntity.Benchmark,
+				Symbols:   &ms.executionEntity.Symbols,
+			}
+			err = ms.execution.Configure(context.Background(), nil, &backtestCfg)
+			if err != nil {
+				return fmt.Errorf("failed to configure execution: %w", err)
+			}
+			err = ms.execution.Run(context.TODO(), ms.executionEntity)
+			if err != nil {
+				return fmt.Errorf("failed to run execution: %w", err)
+			}
+			if rsp.Error == "" {
+				periods, err := ms.execution.StoreDataFrameAndGetPeriods(context.Background(), ms.executionEntity.ID)
+				if err != nil {
+					return fmt.Errorf("failed to store data frame and get periods: %w", err)
+				}
+				for _, period := range *periods {
+					err = ms.session.periods.Store(context.Background(), ms.executionEntity.ID, &period)
+					if err != nil {
+						return fmt.Errorf("failed to store period: %w", err)
+					}
+				}
+			}
+			ms.execution = nil
+			ms.executionEntity = nil
+		case "stop":
+			byteMsg, err = rsp.Encode()
+			if err != nil {
+				return fmt.Errorf("failed to encode message: %w", err)
+			}
+			if err = socket.Write(byteMsg); err != nil {
+				return fmt.Errorf("failed to write message: %w", err)
+			}
+			return nil
+		default:
+			err = errors.New("unknown task")
+		}
+		if err != nil {
+			rsp.Error = err.Error()
+		}
+		byteMsg, err = rsp.Encode()
+		if err != nil {
+			return fmt.Errorf("failed to encode message: %w", err)
+		}
+		if err = socket.Write(byteMsg); err != nil {
+			return fmt.Errorf("failed to write message: %w", err)
+		}
+	}
+}
+
+func (ms *manualSession) Stop(ctx context.Context) error {
+	if err := ms.workers.Stop(ctx); err != nil {
+		return fmt.Errorf("failed to stop workers: %w", err)
+	}
+	return ms.socket.Close()
+}
