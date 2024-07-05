3147a4774286692e9635daf322ae2a0897e05a54
Henrik Nilsson <henrik@lhjnilsson.com>
2024-07-05 21:07:09 +0200

    use upstream minio

--- internal/test_helper/containers.go
+++ internal/test_helper/containers.go
@@ -21,6 +21,7 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/minio"
 	"github.com/testcontainers/testcontainers-go/modules/nats"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -126,10 +127,10 @@ func NATSContainer(t *testing.T, NetworkID string) (ConnectionString string) {
 func MinioContainer(t *testing.T, NetworkID string) (ConnectionString, AccessKey, SecretKey string) {
 	t.Helper()
 
-	container, err := RunContainer(context.TODO(),
+	container, err := minio.RunContainer(context.TODO(),
 		testcontainers.WithImage(MinioImage),
-		WithUsername("minioadmin"),
-		WithPassword("minioadmin"),
+		minio.WithUsername("minioadmin"),
+		minio.WithPassword("minioadmin"),
 		testcontainers.WithEndpointSettingsModifier(func(settings map[string]*network.EndpointSettings) {
 			settings[NetworkID] = &network.EndpointSettings{
 				Aliases:   []string{"minio"},
@@ -235,88 +236,4 @@ func LokiContainerAndLogging(t *testing.T, NetworkID string) (ConnectionString s
 	})
 	log.Logger = zerolog.New(zerolog.MultiLevelWriter(zerolog.NewConsoleWriter(), lokiLogger))
 	return fmt.Sprintf("http://%s:%d", host, port.Int())
-}
-
-// Copied from https://github.com/testcontainers/testcontainers-go/blob/main/modules/minio/minio.go
-// Remove below when new release is out with minio in upstream
-
-const (
-	defaultUser     = "minioadmin"
-	defaultPassword = "minioadmin"
-	defaultImage    = "docker.io/minio/minio:RELEASE.2024-01-16T16-07-38Z"
-)
-
-// MinioContainer represents the Minio container type used in the module
-type minioContainer struct {
-	testcontainers.Container
-	Username string
-	Password string
-}
-
-// WithUsername sets the initial username to be created when the container starts
-// It is used in conjunction with WithPassword to set a user and its password.
-// It will create the specified user. It must not be empty or undefined.
-func WithUsername(username string) testcontainers.CustomizeRequestOption {
-	return func(req *testcontainers.GenericContainerRequest) {
-		req.Env["MINIO_ROOT_USER"] = username
-	}
-}
-
-// WithPassword sets the initial password of the user to be created when the container starts
-// It is required for you to use the Minio image. It must not be empty or undefined.
-// This environment variable sets the root user password for Minio.
-func WithPassword(password string) testcontainers.CustomizeRequestOption {
-	return func(req *testcontainers.GenericContainerRequest) {
-		req.Env["MINIO_ROOT_PASSWORD"] = password
-	}
-}
-
-// ConnectionString returns the connection string for the minio container, using the default 9000 port, and
-// obtaining the host and exposed port from the container.
-func (c *minioContainer) ConnectionString(ctx context.Context) (string, error) {
-	host, err := c.Host(ctx)
-	if err != nil {
-		return "", err
-	}
-	port, err := c.MappedPort(ctx, "9000/tcp")
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%s:%s", host, port.Port()), nil
-}
-
-// RunContainer creates an instance of the Minio container type
-func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomizer) (*minioContainer, error) {
-	req := testcontainers.ContainerRequest{
-		Image:        defaultImage,
-		ExposedPorts: []string{"9000/tcp"},
-		WaitingFor:   wait.ForHTTP("/minio/health/live").WithPort("9000"),
-		Env: map[string]string{
-			"MINIO_ROOT_USER":     defaultUser,
-			"MINIO_ROOT_PASSWORD": defaultPassword,
-		},
-		Cmd: []string{"server", "/data"},
-	}
-
-	genericContainerReq := testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	}
-
-	for _, opt := range opts {
-		opt.Customize(&genericContainerReq)
-	}
-
-	username := req.Env["MINIO_ROOT_USER"]
-	password := req.Env["MINIO_ROOT_PASSWORD"]
-	if username == "" || password == "" {
-		return nil, fmt.Errorf("username or password has not been set")
-	}
-
-	container, err := testcontainers.GenericContainer(ctx, genericContainerReq)
-	if err != nil {
-		return nil, err
-	}
-
-	return &minioContainer{Container: container, Username: username, Password: password}, nil
 }
