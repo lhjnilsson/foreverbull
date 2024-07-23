2ffb0a30e00f63ad80e2cf9ecdebe4858bec0975
Henrik Nilsson <henrik@lhjnilsson.com>
2024-07-23 22:48:38 +0200

    lint

--- client/python/src/foreverbull/__init__.py
+++ client/python/src/foreverbull/__init__.py
@@ -4,5 +4,3 @@ from foreverbull.data import Asset, Assets, Portfolio  # noqa
 from foreverbull.entity.finance import Order  # noqa
 from foreverbull.models import Algorithm, Function, Namespace  # noqa
 from foreverbull.foreverbull import Foreverbull  # noqa
-
-from . import socket  # noqa
