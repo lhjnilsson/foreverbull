141fc9c4f0e08ed37a39a2ce6ccc752d3c62f9d2
Henrik Nilsson <henrik@lhjnilsson.com>
2024-10-28 20:52:09 +0100

    define list executions

--- client/tests/broker.py
+++ client/tests/broker.py
@@ -58,6 +58,11 @@ class Broker:
             )
         )
 
+    def ListExecutions(
+        self, request: backtest_service_pb2.ListExecutionsRequest, context
+    ) -> backtest_service_pb2.ListExecutionsResponse:
+        return backtest_service_pb2.ListExecutionsResponse()
+
     def GetExecution(
         self, request: backtest_service_pb2.GetExecutionRequest, context
     ) -> backtest_service_pb2.GetExecutionResponse:
