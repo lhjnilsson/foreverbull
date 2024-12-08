e3278be61547560e7fe62ba91f13a51b920f7cf1
Henrik Nilsson <henrik@lhjnilsson.com>
2024-12-08 21:25:51 +0100

    register strategy stuff

--- pkg/strategy/module.go
+++ pkg/strategy/module.go
@@ -1,1 +1,23 @@
 package strategy
+
+import (
+	finance_pb "github.com/lhjnilsson/foreverbull/pkg/finance/pb"
+	"github.com/lhjnilsson/foreverbull/pkg/strategy/internal/servicer"
+	"github.com/lhjnilsson/foreverbull/pkg/strategy/pb"
+	"go.uber.org/fx"
+	"google.golang.org/grpc"
+)
+
+var Module = fx.Options( //nolint: gochecknoglobals
+	fx.Provide(
+		func() (finance_pb.MarketdataClient, error) {
+
+			return finance_pb.NewMarketdataClient(nil), nil
+		}),
+	fx.Invoke(
+		func(g *grpc.Server, md finance_pb.MarketdataClient) error {
+			srv := servicer.NewStrategyServer(md)
+			pb.RegisterStrategyServicerServer(g, srv)
+			return nil
+		}),
+)
