29233cd674c7da8c6adc0b1fbcb713b111d7d6be
Henrik Nilsson <henrik@lhjnilsson.com>
2024-12-09 19:03:12 +0100

    add conn to marketdata

--- pkg/strategy/module.go
+++ pkg/strategy/module.go
@@ -1,18 +1,28 @@
 package strategy
 
 import (
+	"fmt"
+
 	finance_pb "github.com/lhjnilsson/foreverbull/pkg/finance/pb"
 	"github.com/lhjnilsson/foreverbull/pkg/strategy/internal/servicer"
 	"github.com/lhjnilsson/foreverbull/pkg/strategy/pb"
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var Module = fx.Options( //nolint: gochecknoglobals
 	fx.Provide(
 		func() (finance_pb.MarketdataClient, error) {
+			conn, err := grpc.NewClient("localhost:50555", grpc.WithTransportCredentials(
+				insecure.NewCredentials(),
+			))
+			if err != nil {
+				return nil, fmt.Errorf("failed to dial: %w", err)
+			}
 
-			return finance_pb.NewMarketdataClient(nil), nil
+			// TO-DO: refactor so we properly can close the connection
+			return finance_pb.NewMarketdataClient(conn), nil
 		}),
 	fx.Invoke(
 		func(g *grpc.Server, md finance_pb.MarketdataClient) error {
